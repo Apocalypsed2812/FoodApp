26c0d53af558452604d8b876283ecedb6ffc421d
Apocalysed <phamhuynhanhtien.12a20.2019@gmail.com>
2022-12-19 16:16:16 +0700

    add view order detail

--- components/Order.js
+++ components/Order.js
@@ -1,10 +1,30 @@
-import React from "react";
-import {Image, Text, View, StyleSheet, TextInput, Button, Alert, TouchableOpacity } from 'react-native'
+import React, {useState} from "react";
+import {Image, Text, View, StyleSheet, TextInput, Modal, Alert, TouchableOpacity } from 'react-native'
+import { or } from "react-native-reanimated";
 
 import SkiiImage from '../assets/skii.png'
 
 function Order(props){
-  const {order} = props
+    const {order} = props
+    const [modalVisible, setModalVisible] = useState(false);
+    // const [name, setName] = useState(false);
+    // const [address, setAddress] = useState(false);
+    // const [total, setTotal] = useState(false);
+
+    const showModalView = (e) => {
+        // setName(e.target.getAttribute("data-name"))
+        // setAddress(e.target.getAttribute("data-address"))
+        // setTotal(e.target.getAttribute("data-total"))
+        // setName(order.name)
+        // setAddress(order.address)
+        // setTotal(order.total)
+        setModalVisible(true)
+    }
+
+    const closeModalView = () => {
+      setModalVisible(false)
+    }
+
     return (
         <>
           <View style={styles.wrapper}>
@@ -26,7 +46,7 @@ function Order(props){
                     <Text>Thành tiền: </Text>
                     <Text style={styles.totalPrice}>{order.total}</Text>
                   </Text>
-                  <TouchableOpacity>
+                  <TouchableOpacity onPress={showModalView} data-name={order.name} data-address={order.address} data-total={order.total}>
                     <Text style={styles.detail}> 
                       Chi tiết
                     </Text>
@@ -34,6 +54,42 @@ function Order(props){
                 </View>
             </View>
           </View>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <TouchableOpacity style={styles.modal__overlay} onPress={() => setModalVisible(!modalVisible)}>
+            </TouchableOpacity>
+              <View style={styles.modal__container}>
+                <Text style={styles.modal__container_heading}>Chi tiết đơn hàng</Text>
+                <View style={styles.modal__form_group}>
+                  <Text>Tên Khách Hàng: </Text>
+                  <Text>{order.name}</Text>
+                </View>
+                <View style={styles.modal__form_group}>
+                  <Text>Địa Chỉ: </Text>
+                  <Text>{order.address}</Text>
+                </View>
+                <View style={styles.modal__form_group}>
+                  <Text>Số Điện Thoại: </Text>
+                  <Text>{order.phone}</Text>
+                </View>
+                <View style={styles.modal__form_group}>
+                  <Text>Tổng Tiền: </Text>
+                  <Text>{order.total}</Text>
+                </View>
+                <View style={styles.modal__form_group}>
+                  <Text>Trạng Thái: </Text>
+                  <Text>{order.status}</Text>
+                </View>
+                <Text style={styles.button} onPress={closeModalView}>Đóng</Text>
+              </View>
+          </Modal>
         </>
     )
 }
@@ -90,6 +146,68 @@ const styles = StyleSheet.create({
       color: 'green',
       paddingHorizontal: 8,
       paddingVertical: 4
+    },
+    modal__container: {
+      width: '100%',
+      backgroundColor: '#fff',
+      position: 'absolute',
+      bottom: 0,
+      height: 450,
+      borderColor: '#ccc',
+      borderWidth: 1,
+      display: 'flex',
+      alignItems: 'center',
+      zIndex: 1,
+    },
+    modal__overlay: {
+      backgroundColor: 'transparent',
+      position: 'absolute',
+      bottom: 450,
+      top: 0,
+      left: 0,
+      right: 0,
+    },
+    modal__container_heading: {
+      width: '100%',
+      textAlign: 'center',
+      lineHeight: 40,
+      color: 'green',
+      fontSize: 20,
+      fontWeight: '600',
+      marginVertical: 30,
+    },
+    input: {
+      height: 40,
+      margin: 12,
+      width: '90%',
+      paddingVertical: 10,
+      paddingLeft: 24,
+      borderRadius: 24,
+      backgroundColor: '#fff',
+      color: 'black',
+      outline: 'none',
+      borderColor: '#ccc',
+      borderWidth: 1,
+    },
+    button: {
+      height: 40,
+      marginTop: 50,
+      paddingVertical: 10,
+      paddingHorizontal: 100,
+      borderRadius: 24,
+      backgroundColor: '#fff',
+      color: 'black',
+      outline: 'none',
+      borderColor: 'gray',
+      borderWidth: 1,
+      textTransform: 'uppercase',
+      color: 'gray',
+    },
+    modal__form_group: {
+      display: 'flex',
+      flexDirection: 'row',
+      marginBottom: 20,
+      fontSize: 30,
     }
 })
 
